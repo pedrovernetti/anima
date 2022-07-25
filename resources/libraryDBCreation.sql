b8c82d12235fbbe061b16e9501207bb9d8d29db5
pedrovernetti <pedrovernetti@gmail.com>
2022-07-24 22:27:57 -0300

    gpl header added to resources

--- resources/libraryDBCreation.sql
+++ resources/libraryDBCreation.sql
@@ -1,3 +1,25 @@
+/*//////////////////////////////////////////////////////////////////////////
+//                                                                        //
+//                                ANIMA                                   //
+//           Copyright (C) 2022-2022 Pedro Vernetti Gonçalves             //
+//                                                                        //
+//  This program is free software: you can redistribute it and/or modify  //
+//  it under the terms of the GNU General Public License as published by  //
+//   the Free Software Foundation, either version 3 of the License, or    //
+//                 (at your option) any later version.                    //
+//                                                                        //
+//     This program is distributed in the hope that it will be useful,    //
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of     //
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      //
+//             GNU General Public License for more details.               //
+//                                                                        //
+//   You should have received a copy of the GNU General Public License    //
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.  //
+//                                                                        //
+//////////////////////////////////////////////////////////////////////////*/
+
+/* This script creates a new SQLite library database for Anima Player */
+
 CREATE TABLE IF NOT EXISTS artists
 (
     id INTEGER PRIMARY KEY NOT NULL,
